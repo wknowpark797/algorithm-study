673bc378fe8b9603c8350d8f3fa24b7685fca22b
wknowpark797 <wknowpark797@gmail.com>
2022-07-09 14:45:07 +0900

    [백준] 5524 - 입실 관리 : 테스트 링크 수정

--- baekjoon/5524.js
+++ baekjoon/5524.js
@@ -4,7 +4,7 @@
 */
 
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = fs.readFileSync('input.txt').toString().trim().split('\n');
 
 input.shift();
 
